4ecf958073af3386c308886b04cff68cc2c5acf6
jhulia <jhuliavitoria226@gmail.com>
2026-03-12 17:12:34 -0300

    segundo commit

--- classes.py
+++ classes.py
@@ -1,1 +1,26 @@
-print("ai,ai,ai,ai,ai")
+class Jogador:
+    def __init__(self,nome,id,idade,numero_de_telefone,email,historico):
+        self.nome = nome
+        self.id = id
+        self.idade = idade
+        self.numero_de_telefone = numero_de_telefone
+        self.email = email
+        self.historico = []
+        
+
+class Time:
+    def __init__(self,nome,id,esporte,brasao,historico):
+        self.nome = nome
+        self.id = id
+        self.esporte = esporte
+        self.brasao = brasao
+        self.jogadores = []
+        self.historico = historico
+        
+
+    def adicionarJogador (self,jogador):
+        self.jogadores.append(jogador)
+
+    def apresentar_jogadores(self):
+        nomes = [jogador.nome for jogador in self.jogadores]
+        return "Os jogadores são: " + ", ".join(nomes)
--- projeto.py
+++ projeto.py
@@ -1,1 +1,10 @@
-print("gaubim")
+from classes import Jogador
+from classes import Time
+
+sahur = Jogador("sahur", 1, 18, 12234555, "gabuim")
+
+garopaba_mulets = Time("garopaba mulets", 1, "basquete", "jpg", "historico legal" )
+
+garopaba_mulets.adicionarJogador(sahur)
+
+print(garopaba_mulets.apresentar_jogadores())
